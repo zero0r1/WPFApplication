678d4da870e38c9a4a7e7f6b832d74d998f7804c
zero0r1 <leahdizonth@gmail.com>
2017-09-11 23:29:52 +0800

    Commit

--- WPFlayout/obj/Debug/WPFlayout_Content.g.i.cs
+++ WPFlayout/obj/Debug/WPFlayout_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("%e5%ad%a6%e4%b9%a0%e6%96%87%e6%a1%a3/%e6%b4%be%e7%94%9f%e8%87%aa%20headereditemsc" +
-    "ontrol%20%e6%8e%a7%e4%bb%b6.txt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("%e5%ad%a6%e4%b9%a0%e6%96%87%e6%a1%a3/%e5%86%85%e5%ae%b9%e6%8e%a7%e4%bb%b6.txt")]
 
 
