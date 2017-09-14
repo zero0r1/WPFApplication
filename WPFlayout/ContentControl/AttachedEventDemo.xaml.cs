ced2c3192b15d5ea4fecf1d717adba1e047470e2
zero0r1 <leahdizonth@gmail.com>
2017-09-14 23:40:41 +0800

    Commit

---
+++ WPFlayout/ContentControl/AttachedEventDemo.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFlayout.ContentControl
+{
+    /// <summary>
+    /// AttachedEventDemo.xaml 的交互逻辑
+    /// </summary>
+    public partial class AttachedEventDemo : Window
+    {
+        public AttachedEventDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button)
+            {
+                MessageBox.Show(((Button)e.Source).Content.ToString());
+            }
+        }
+    }
+}
---
+++ WPFlayout/ContentControl/UnderstandingDp.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFlayout.ContentControl
+{
+    /// <summary>
+    /// UnderstandingDp.xaml 的交互逻辑
+    /// </summary>
+    public partial class UnderstandingDp : Window
+    {
+        public UnderstandingDp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWindowsSize_Click(object sender, RoutedEventArgs e)
+        {
+            this.FontSize = 20;
+        }
+
+        private void btnButtonSize_Click(object sender, RoutedEventArgs e)
+        {
+            txt.FontSize = 10;
+        }
+    }
+}
